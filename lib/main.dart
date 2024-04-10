34422e666c6599b9189dea25409396ecfa4f4557
mokhles404 <mokhleslajimi@gmail.com>
2024-04-11 00:30:31 +0100

    code of seance3

--- lib/main.dart
+++ lib/main.dart
@@ -3,93 +3,227 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double val1 = 0;
-    double val2 = 0;
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
+
+      home: Page1(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+class Page2 extends StatefulWidget {
+  const Page2({super.key});
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
+
+  int count =0;
+
+
+  Widget build(BuildContext context) {
+    double width =MediaQuery.sizeOf(context).width;
+    double height =MediaQuery.sizeOf(context).height;
+    return Scaffold(
+      backgroundColor: Colors.cyan,
         appBar: AppBar(
-          title: Text("test title"),
+          title: Text("Count"),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                "Calcul",
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 36,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 18),
-                  decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(.5),
-                    borderRadius: BorderRadius.circular(36)
-                  ),
-                  child: TextField(
-                    onChanged: (String valeur) {
-                      val1 = double.tryParse(valeur)!;
-                    },
-                  )),
-              SizedBox(
-                height: 40,
-              ),
-              TextField(
-                style: TextStyle(fontSize: 26),
-                onChanged: (String valeur) {
-                  val2 = double.tryParse(valeur)!;
-                },
+      body: Container(
+        width: width,
+        height: height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-                decoration: InputDecoration(
-                  icon: Icon(Icons.phone)      ,
-                  fillColor: Colors.white70 ,
-                  filled: true,
-                  hintText: "Valeur2"    ,
-                  prefixIcon: Icon(Icons.access_time),
-                  suffixIcon: Icon(Icons.add_business)
+            Text(count.toString(),style: TextStyle(fontSize: 36),),
 
 
-                ),
-              ),
-              SizedBox(
-                height: 36,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 18,
-                  maximumSize: Size(250,66),
-                  backgroundColor: Colors.red
-                ),
-                onPressed: () {
-                  print(val1 + val2);
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.print),
-                    Text("Calculate")
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("Test");
-          },
-          child: Icon(Icons.home),
+
+
+            ElevatedButton(onPressed: (){
+
+                  setState(() {
+                    count++;
+                  });
+
+
+            }, child: Icon(Icons.add)),
+            ElevatedButton(onPressed: (){
+
+
+
+              setState(() {
+                count--;
+              });
+
+            }, child: Icon(Icons.minimize)),
+
+
+            ElevatedButton(onPressed: (){
+           Navigator.pop(context);
+
+            }, child: Text("Back")),
+
+
+          ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Page1 extends StatefulWidget {
+   Page1({super.key});
+
+  @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+   TextEditingController text1 =TextEditingController();
+
+   TextEditingController text2 =TextEditingController();
+
+    String error="";
+    int count =0;
+    bool show=true;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Login",style: TextStyle(fontSize: 38,fontWeight: FontWeight.bold),),
+
+            TextField(
+              controller: text1,
+              decoration: InputDecoration(
+                labelText: "Login",
+                fillColor: Colors.grey,
+                filled: true
+              ),
+
+            ),
+            SizedBox(height: 16,),
+            TextField(
+              obscureText: show,
+              controller: text2,
+              decoration: InputDecoration(
+                labelText: "Password",
+                labelStyle: TextStyle(fontSize: 22),
+                fillColor: Colors.grey,
+                filled: true,
+                suffixIcon: IconButton(onPressed: () {
+
+                  show=!show;
+                  setState(() {
+
+                  });
+
+                }, icon: Icon(Icons.remove_red_eye),
+
+                )
+              ),
+
+            ),
+            SizedBox(height: 16,),
+
+            Text(error,style: TextStyle(fontSize: 18,color: Colors.red),),
+
+            ElevatedButton(onPressed: (){
+              setState(() {
+                error="";
+              });
+              if(!text1.text.contains("@")){
+                print("this not  correct email");
+
+                setState(() {
+                  error="this not correct email";
+              });
+                return;
+              }
+
+              if(text1.text!="login@gmail.com"){
+                setState(() {
+                  error="login Incorrect";
+                });
+                return;
+              }
+              if(text2.text!="12345"){
+                setState(() {
+                  error="Password Incorrect";
+                });
+                return;
+              }
+
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Page2()),
+              );
+
+
+
+
+            }, child: Text("Login")),
+
+          SizedBox(height: 16,),
+
+            ElevatedButton(onPressed: (){
+              print("***********");
+              print(error);
+              print("***********");
+
+            }, child: Text("Clear")),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
